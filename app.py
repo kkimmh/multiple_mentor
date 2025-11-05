5cc8a006a354226ff064343d7a50cdb4aac9e7ab
kkimmh <muhyen10211021@gmail.com>
2025-11-05 22:52:09 +0900

    first deploy

--- app.py
+++ app.py
@@ -269,3 +269,6 @@ if __name__ == "__main__":
     socketio = SocketIO(app, cors_allowed_origins="*")
     port = int(os.environ.get("PORT", 5000))
     socketio.run(app, host="0.0.0.0", port=port, debug=False)
+
+
+
